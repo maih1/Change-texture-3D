56ce56e7cabc31b18cccbbee28dbd1d6f147b22f
maih1 <hoangmai.tq2000@gmail.com>
2021-07-08 16:40:05 +0700

    udate server and home

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const id = ["chair", "shoes"]
 app.use('/data', express.static(pwd + '/data'));
 app.use(express.static(path.join(__dirname, 'src')));
 
+// app.use(express.static(path.join(__dirname, 'style')));
+app.use('/style', express.static(pwd + '/style'));
+
+
 for (let i of id) {
   // app.use('/' , express.static(pwd + '/data'))
   app.get(['/' + ':id'], (req, res) => {
@@ -19,8 +23,12 @@ for (let i of id) {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/src/home/index.html");
+  res.sendFile(__dirname + "/main.html");
 })
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + "/src/home/index.html");
+// })
 
 app.listen(port, () => {
   console.log(__dirname)
